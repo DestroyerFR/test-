2c1fb8d5f1c4beea2ff72a72adafe8125b32e931
maximegeekk <37552555+maximegeekk@users.noreply.github.com>
2018-05-04 19:31:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -356,3 +356,20 @@ bot.on('message', message => {
     }
 
 });
+
+
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+
+module.exports.help = {
+  name: "say"
+}
